105bfee00214ff2f5378f5f80a80355807d66bd3
Abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-10-11 14:55:33 +0100

    Add GenericDao interface for generic data access operations

---
+++ src/main/java/org/hospital/sbitari/dao/GenericDao.java
@@ -0,0 +1,19 @@
+package org.hospital.sbitari.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface GenericDao<T, ID> {
+
+    Optional<T> findById(ID id);
+
+    List<T> findAll();
+
+    T save(T entity);
+
+    T update(T entity);
+
+    void delete(T entity);
+
+    void deleteById(ID id);
+}
